59e87b509448e41fd3ac84ec59fa10eeb7ca85c1
Anna <Laluna-93@yandex.ru>
2022-12-10 00:14:34 +0400

    feat: added the pets-page section and the functionality for adding pet cards

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,45 @@
 import './style.scss';
+
+const pathPets = './assets/json/pets.json';
+const petsSlider = document.querySelector('.pets-page_slider');
+
+console.log(petsSlider);
+
+const getPets = async (path: string) => {
+  const res = await fetch(path);
+  const data = await res.json();
+
+  createCards(data.slice(0, 3));
+
+  return data;
+};
+
+function createCards(pets: any) {
+  return pets.forEach((card: any) => {
+    const wrapperCard = document.createElement('div');
+    const imgPet = document.createElement('img');
+    const titlePet = document.createElement('h3');
+    const btnPet = document.createElement('button');
+
+    wrapperCard.classList.add('wrapperCard');
+    imgPet.classList.add('imgPet');
+    titlePet.classList.add('titlePet');
+    btnPet.classList.add('btnPet');
+
+    imgPet.src = card.img;
+    titlePet.textContent = card.name;
+    btnPet.textContent = 'Learn more';
+    btnPet.onclick = () => {
+      console.log('click', card.name);
+    };
+
+    wrapperCard.append(imgPet);
+    wrapperCard.append(titlePet);
+    wrapperCard.append(btnPet);
+    petsSlider?.append(wrapperCard);
+
+    console.log(pets, 'pets');
+  });
+}
+
+getPets(pathPets);
