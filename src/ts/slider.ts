72119b37779c4a9964a84524e9f5f3692e92c19f
Anna <Laluna-93@yandex.ru>
2022-12-13 00:00:42 +0400

    fix: fixed slide movement functionality

--- src/ts/slider.ts
+++ src/ts/slider.ts
@@ -4,58 +4,55 @@ import { arrayPets } from './variables';
 
 export function slider() {
   const pageLocal = localStorage.getItem('page');
-  let numberPage: any;
-  let count = 0;
+  let numberPage: number = 0;
 
-  if (pageLocal) {
-    numberPage = JSON.parse(pageLocal);
-    console.log(pageLocal);
-  } else {
-    numberPage = 1;
-  }
+  pageLocal ? numberPage = JSON.parse(pageLocal) : numberPage = 1;
 
   btnLeftSlider.onclick = () => {
-    const res = +`${(arrayPets.length - 1) * 990}`;
-
-    count > res ? count += 990 : count -= 2970;
-
-    count += 990;
-
     numberPage > 1 ? numberPage -= 1 : numberPage = arrayPets.length;
-    activeSlide(numberPage, count);
+    activeSlide(numberPage);
     localStorage.setItem('page', `${numberPage}`);
+    moveSlides();
   };
 
   btnRightSlider.onclick = () => {
-    const res = +`-${(arrayPets.length - 1) * 990}`;
-
-    count > res ? count -= 990 : count += 2970;
-
-    // count -= 990;
-    console.log(res, count, 'counts');
-
     numberPage < arrayPets.length ? numberPage += 1 : numberPage = 1;
-    activeSlide(numberPage, count);
+    activeSlide(numberPage);
     localStorage.setItem('page', `${numberPage}`);
+    moveSlides();
   };
+
+  console.log(numberPage, 'test');
 
   return editNumberPage(numberPage);
 }
 
-function activeSlide(numberPage:number, count: number) {
-  console.log(arrayPets.length, 'a');
-
+function activeSlide(numberPage: number) {
   arrayPets.forEach((slide) => {
     slide.classList.remove('active');
+
     if (slide.id === `${numberPage}`) {
       slide.classList.add('active');
     }
+    // else {
+    //   slide.style.transform = `translateX(${(-990)}px)`;
+    // }
 
-    if (!slide.classList.contains('active')) {
-      slide.style.transform = `translateX(${(count - 990)}px)`;
-    }
+    // if (slide.classList.contains('active')) {
+    //   slide.style.transform = `translateX(${(0)}px)`;
+    // }
 
-    slide.style.transform = `translateX(${count}px)`;
+    // slide.style.transform = `translateX(${-990}px)`;
+    // slide.style.transition = '4s';
   });
 }
-// вычислить максимальное число и сделать ограничитель обнуляя счетчик при его достижении
+
+function moveSlides() {
+  arrayPets.forEach((slide) => {
+    if (slide.classList.contains('active') && slide.id > 1) {
+      const firstSlide = (arrayPets.shift());
+
+      arrayPets.push(firstSlide);
+    }
+  });
+}
