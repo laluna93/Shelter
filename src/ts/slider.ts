667f30409bad435523d55f7ebe571fc27eb92f2c
Anna <Laluna-93@yandex.ru>
2022-12-12 18:46:19 +0400

    feat: added functionality for slides

--- src/ts/slider.ts
+++ src/ts/slider.ts
@@ -5,6 +5,7 @@ import { arrayPets } from './variables';
 export function slider() {
   const pageLocal = localStorage.getItem('page');
   let numberPage: any;
+  let count = 0;
 
   if (pageLocal) {
     numberPage = JSON.parse(pageLocal);
@@ -14,31 +15,47 @@ export function slider() {
   }
 
   btnLeftSlider.onclick = () => {
+    const res = +`${(arrayPets.length - 1) * 990}`;
+
+    count > res ? count += 990 : count -= 2970;
+
+    count += 990;
+
     numberPage > 1 ? numberPage -= 1 : numberPage = arrayPets.length;
-    activeSlide(numberPage);
+    activeSlide(numberPage, count);
     localStorage.setItem('page', `${numberPage}`);
   };
 
   btnRightSlider.onclick = () => {
+    const res = +`-${(arrayPets.length - 1) * 990}`;
+
+    count > res ? count -= 990 : count += 2970;
+
+    // count -= 990;
+    console.log(res, count, 'counts');
+
     numberPage < arrayPets.length ? numberPage += 1 : numberPage = 1;
-    activeSlide(numberPage);
+    activeSlide(numberPage, count);
     localStorage.setItem('page', `${numberPage}`);
   };
 
   return editNumberPage(numberPage);
 }
 
-function activeSlide(numberPage:number) {
-  console.log(numberPage);
-  const val = numberPage - 1;
+function activeSlide(numberPage:number, count: number) {
+  console.log(arrayPets.length, 'a');
 
   arrayPets.forEach((slide) => {
     slide.classList.remove('active');
     if (slide.id === `${numberPage}`) {
       slide.classList.add('active');
-      slide.style.transform = `translateX(${((990))}px)`;
     }
 
-    slide.style.transform = `translateX(${((val * -990))}px)`;
+    if (!slide.classList.contains('active')) {
+      slide.style.transform = `translateX(${(count - 990)}px)`;
+    }
+
+    slide.style.transform = `translateX(${count}px)`;
   });
 }
+// вычислить максимальное число и сделать ограничитель обнуляя счетчик при его достижении
