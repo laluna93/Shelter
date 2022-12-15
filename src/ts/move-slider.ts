e58f227491619df92d36a78a29ae0f2657e7663a
Anna <Laluna-93@yandex.ru>
2022-12-16 02:28:35 +0400

    refactor: changes have been made to the structure of the slider code

--- src/ts/move-slider.ts
+++ src/ts/move-slider.ts
@@ -2,17 +2,7 @@ import { btnLeftSlider, btnRightSlider, petsSlider } from './selectors';
 
 export function moveSlider(numberPage: number, classActive:string, classSlider:string) {
   [...petsSlider.children].forEach((e) => {
-    if (classSlider === 'right') {
-      e.classList.remove('left');
-      e.classList.remove('active-left');
-    } else {
-      e.classList.remove('right');
-      e.classList.remove('active-right');
-    }
-
-    e.classList.remove('active');
-    e.classList.remove(classActive);
-    e.classList.remove(classSlider);
+    removeClassSlides(e, classActive, classSlider);
 
     if (e.id === `${numberPage}`) {
       e.classList.add('active');
@@ -22,20 +12,40 @@ export function moveSlider(numberPage: number, classActive:string, classSlider:s
     }
 
     if (e.classList.contains('active') && classSlider === 'left') {
-      btnRightSlider.disabled = true;
-
-      e.addEventListener('animationend', () => {
-        petsSlider.append(petsSlider.firstElementChild!);
-        btnRightSlider.disabled = false;
-      }, { once: true });
+      moveSlides(e, 'left');
     }
 
     if (e.classList.contains('active') && classSlider === 'right') {
-      btnLeftSlider.disabled = true;
       petsSlider.prepend(petsSlider.lastElementChild!);
-      e.addEventListener('animationend', () => {
-        btnLeftSlider.disabled = false;
-      }, { once: true });
+      moveSlides(e, 'right');
     }
   });
+}
+
+function moveSlides(e:Element, classStyle: string) {
+  btnRightSlider.disabled = true;
+  btnLeftSlider.disabled = true;
+
+  e.addEventListener('animationend', () => {
+    if (classStyle === 'left') {
+      petsSlider.append(petsSlider.firstElementChild!);
+    }
+
+    btnRightSlider.disabled = false;
+    btnLeftSlider.disabled = false;
+  }, { once: true });
+}
+
+function removeClassSlides(e:Element, classActive:string, classSlider:string) {
+  if (classSlider === 'right') {
+    e.classList.remove('left');
+    e.classList.remove('active-left');
+  } else {
+    e.classList.remove('right');
+    e.classList.remove('active-right');
+  }
+
+  e.classList.remove('active');
+  e.classList.remove(classActive);
+  e.classList.remove(classSlider);
 }
