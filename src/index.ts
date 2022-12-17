3fb9849733aa0669f5d353f2141a932812895d54
Anna <Laluna-93@yandex.ru>
2022-12-17 08:27:22 +0400

    feat: added tracking of screen width changes

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,12 @@ import { pathPets } from './ts/variables';
 
 getPets(pathPets);
 slider();
+
+const nowWidth = window.screen.availWidth;
+const windowWidth = window.matchMedia('(max-width: 600px)');
+
+windowWidth.onchange = () => {
+  console.log(windowWidth, '1s');
+};
+
+console.log(nowWidth);
