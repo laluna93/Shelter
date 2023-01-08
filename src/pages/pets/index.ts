2c83eb7c0bba42ec2970d3b7dc04f7ea38ce77b1
Anna <Laluna-93@yandex.ru>
2023-01-08 14:03:44 +0400

    feat: added functionality to open the burger menu on the pets page

--- src/pages/pets/index.ts
+++ src/pages/pets/index.ts
@@ -1,6 +1,8 @@
+import { viewBurger } from '../../assets/ts/burger-menu';
 import { getLengthSlides } from '../../assets/ts/slider/length-slide';
 import './style.scss';
 import { btnPets } from './ts/pagination/control-pagination';
 
 btnPets();
 getLengthSlides();
+viewBurger();
