7a03b657475dc47224590e3e0bb21d149cd0b018
Anna <Laluna-93@yandex.ru>
2023-01-08 13:29:15 +0400

    feat: added functionality for changing the number of pets on the page when changing the screen width

--- src/assets/ts/slider/length-slide.ts
+++ src/assets/ts/slider/length-slide.ts
@@ -1,33 +1,68 @@
-import { desktop, mobile, pets, tablet } from '../variables';
+import { changeNumberPage } from '../../../pages/pets/ts/number-page';
+import { changeActiveBtn } from '../../../pages/pets/ts/pagination/active-btn-pagination';
+import { createCardsPets } from '../../../pages/pets/ts/pagination/pagination';
+import { arrayBtn, desktop, mobile, pagination, pets, petsSlider, tablet, wrapperPets } from '../variables';
 import { slider } from './btn-slider';
-import { renderSlides } from './slider';
 
 tablet.addEventListener('change', getLengthSlides);
 desktop.addEventListener('change', getLengthSlides);
 mobile.addEventListener('change', getLengthSlides);
 export function getLengthSlides() {
-  let lengthSlide = 0;
   const path = document.location.pathname;
+  let result: number = 0;
 
-  console.log(path, 'p');
+  if (path === '/index.html') {
+    result = changeLengthSlider();
+  } else {
+    result = changeLengthPagination();
+  }
+
+  console.log(result, 'r');
+
+  return result;
+}
+
+function changeLengthPagination() {
+  let lengthSlide = 0;
+
+  changeNumberPage(wrapperPets, 1);
+  changeActiveBtn(arrayBtn, true, true, false, false);
+  if (desktop.matches) {
+    lengthSlide = 8;
+  }
+
+  if (tablet.matches) {
+    lengthSlide = 6;
+  }
+
+  if (mobile.matches) {
+    lengthSlide = 3;
+  }
+
+  createCardsPets(pets, lengthSlide, pagination);
+
+  return lengthSlide;
+}
+
+function changeLengthSlider() {
+  let lengthSlide = 0;
 
   if (desktop.matches) {
     lengthSlide = 3;
-    renderSlides(pets, lengthSlide);
     slider(lengthSlide);
   }
 
   if (tablet.matches) {
     lengthSlide = 2;
-    renderSlides(pets, lengthSlide);
     slider(lengthSlide);
   }
 
   if (mobile.matches) {
     lengthSlide = 1;
-    renderSlides(pets, lengthSlide);
     slider(lengthSlide);
   }
+
+  createCardsPets(pets, lengthSlide, petsSlider);
 
   return lengthSlide;
 }
